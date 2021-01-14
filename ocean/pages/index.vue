0931ad782cd2384dacd6da07cafd1b0474f14211
“liyibass” <liyibass@mirrormedia.mg>
2021-01-14 15:22:34 +0800

    feat: add loadingMask in main page.

--- ocean/pages/index.vue
+++ ocean/pages/index.vue
@@ -1,5 +1,10 @@
 <template>
     <div>
+        <transition name="fade">
+            <div class="loadingMask" v-if="isLoading">
+                <img src="~/assets/images/loading.gif" alt="" />
+            </div>
+        </transition>
         <ScrollHero />
         <VolumeHint />
         <Beach />
@@ -31,7 +36,51 @@ export default {
         Video,
         Final,
     },
+    data() {
+        return {
+            isLoading: true,
+        }
+    },
+    // mounted() {
+    //     this.$nextTick(() => {
+    //         console.log('ready load')
+    //         // this.isLoading = false
+    //     })
+    // },
+    created() {
+        // would work in 'ready', 'attached', etc.
+        window.addEventListener('load', () => {
+            console.log('ready load')
+            this.isLoading = false
+        })
+    },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.loadingMask {
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background: black;
+    opacity: 0.9;
+    z-index: 999;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    img {
+        width: 50px;
+    }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 1s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+}
+</style>
