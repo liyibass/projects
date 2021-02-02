03ddc626dc74cfc37b252bbe0bd94ffa2c753d49
“liyibass” <liyibass@mirrormedia.mg>
2021-02-02 15:47:13 +0800

    feat: mute ocean when play video

--- ocean/components/Video.vue
+++ ocean/components/Video.vue
@@ -2,19 +2,20 @@
     <div class="Video" ref="videoRef" :style="{ height: `${wrapperHeight}px` }">
         <div class="Video__fix_wrapper">
             <div class="Video__fix_wrapper_youtube">
-                <!-- <youtube
+                <youtube
                     :video-id="videoId"
                     ref="youtube"
                     :fitParent="true"
                     :resize="true"
-                    width="360"
-                ></youtube> -->
+                ></youtube>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import 'intersection-observer'
+import scrollama from 'scrollama'
 export default {
     data() {
         return {
@@ -28,10 +29,15 @@ export default {
     },
     methods: {
         playVideo() {
+            const oceanAudio = document.querySelector('.oceanAudio')
+            oceanAudio.pause()
             console.log('video played')
             this.player.playVideo()
         },
         pauseVideo() {
+            const oceanAudio = document.querySelector('.oceanAudio')
+            oceanAudio.play()
+
             console.log('video paused')
             this.player.pauseVideo()
         },
@@ -51,44 +57,64 @@ export default {
             this.isMobile = true
         }
 
-        // // -------------------------------------------------------
-        // // Handle fix component,hover by next component
-        // // need to define data.wrapperHeight
-        // // and assign it to parent's height
-        // let fixWrapperDOM
-        // const nextComponentClass = '.Final'
-        // const currentComponentWrapperClass = '.Video__fix_wrapper'
+        // instantiate the scrollama
+        const stickyYoutubeScroller = scrollama()
+        const youtubeDOM = document.querySelector('.Video__fix_wrapper_youtube')
+        // setup the instance, pass callback functions
+        stickyYoutubeScroller
+            .setup({
+                step: '.Video',
+                offset: 0,
+            })
+            .onStepEnter((response) => {
+                // { element, index, direction }
+                youtubeDOM.style.position = 'sticky'
+                this.playVideo()
+            })
+            .onStepExit((response) => {
+                // { element, index, direction }
+                youtubeDOM.style.position = 'absolute'
+                this.pauseVideo()
+            })
 
-        // const fixAndHoverScene = this.$scrollmagic
-        //     .scene({
-        //         triggerElement: nextComponentClass,
-        //         offset: 0,
-        //         triggerHook: 1,
-        //         duration: 1000,
-        //     })
-        //     .on('enter', (e) => {
-        //         fixWrapperDOM = document.querySelector(
-        //             currentComponentWrapperClass
-        //         )
-        //         // get wrapper's height,then assign to parient
-        //         this.wrapperHeight = fixWrapperDOM.clientHeight
-        //         // fix css
-        //         fixWrapperDOM.style.position = 'fixed'
-        //         fixWrapperDOM.style.width = '100%'
-        //         fixWrapperDOM.style.bottom = '0px'
+        // setup resize event
+        window.addEventListener('resize', stickyYoutubeScroller.resize)
 
-        //         this.playVideo()
-        //     })
-        //     .on('leave', (e) => {
-        //         // unfix css
-        //         fixWrapperDOM.style.position = 'relative'
-        //         this.pauseVideo()
-        //     })
-        // // .addIndicators({ name: 'fixStoryScene' })
+        // -------------------------------------------------------
+        // Handle fix component,hover by next component
+        // need to define data.wrapperHeight
+        // and assign it to parent's height
+        let fixWrapperDOM
+        const nextComponentClass = '.Final'
+        const currentComponentWrapperClass = '.Video__fix_wrapper'
 
-        // // -------------------------------------------------------
+        const fixAndHoverScene = this.$scrollmagic
+            .scene({
+                triggerElement: nextComponentClass,
+                offset: 0,
+                triggerHook: 1,
+                duration: 1000,
+            })
+            .on('enter', (e) => {
+                fixWrapperDOM = document.querySelector(
+                    currentComponentWrapperClass
+                )
+                // get wrapper's height,then assign to parient
+                this.wrapperHeight = fixWrapperDOM.clientHeight
+                // fix css
+                fixWrapperDOM.style.position = 'fixed'
+                fixWrapperDOM.style.width = '100%'
+                fixWrapperDOM.style.bottom = '0px'
+            })
+            .on('leave', (e) => {
+                // unfix css
+                fixWrapperDOM.style.position = 'relative'
+            })
+        // .addIndicators({ name: 'fixStoryScene' })
 
-        // this.$scrollmagic.addScene([fixAndHoverScene])
+        // -------------------------------------------------------
+
+        this.$scrollmagic.addScene([fixAndHoverScene])
     },
 }
 </script>
@@ -101,17 +127,16 @@ export default {
     &__fix_wrapper {
         position: relative;
 
-        height: 100vh;
+        height: 200vh;
         width: 100%;
         background: black;
         // color: white;
 
-        display: flex;
-        justify-content: center;
-        align-items: center;
         &_youtube {
+            position: absolute;
+            top: 0;
             width: 100%;
-            // height: 80%;
+            height: 100vh;
         }
     }
 }
